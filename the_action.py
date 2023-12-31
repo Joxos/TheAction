f38ece1004ba2806b15943878c906b484b12a063
Joxos <johndoe@example.com>
2023-12-31 10:41:38 +0800

    add selection

--- the_action.py
+++ the_action.py
@@ -18,16 +18,30 @@ MARGIN = 2
 SCREEN_WIDTH = (WIDTH + MARGIN) * COLUMN_COUNT + MARGIN
 SCREEN_HEIGHT = (HEIGHT + MARGIN) * ROW_COUNT + MARGIN
 
+def mix_color(a,b):
+    return ((a[0]+b[0])//2,(a[1]+b[1])//2,(a[2]+b[2]//2))
 
 class Game(arcade.Window):
+    def grid_to_central_coordinate(self, row, column):
+        x = column * (WIDTH + MARGIN) + (WIDTH / 2 + MARGIN)
+        y = row * (HEIGHT + MARGIN) + (HEIGHT / 2 + MARGIN)
+        return [x, y]
+
+    def coordinate_to_grid(self, x, y):
+        column = int(x // (WIDTH + MARGIN))
+        row = int(y // (HEIGHT + MARGIN))
+        return [row, column]
+
     def get_cell_color(self, row, column):
-        return self.color[self.grid[row][column]]
+        return self.height_color[self.grid_height[row][column]]
 
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
-        self.grid = generate_map(ROW_COUNT, COLUMN_COUNT, BIOME_STEP)
+
+        self.grid_height = generate_map(ROW_COUNT, COLUMN_COUNT, BIOME_STEP)
+
         self.background_color = arcade.color.BLACK
-        self.color = [
+        self.height_color = [
             arcade.color.BLACK,
             arcade.color.GREEN,
             arcade.color.YELLOW_GREEN,
@@ -35,6 +49,8 @@ class Game(arcade.Window):
             arcade.color.ORANGE,
             arcade.color.RED,
         ]
+
+        self.grid_selected = None
 
         # One dimensional list of all sprites in the two-dimensional sprite list
         self.grid_sprite_list = arcade.SpriteList()
@@ -48,13 +64,10 @@ class Game(arcade.Window):
         for row in range(ROW_COUNT):
             self.grid_sprites.append([])
             for column in range(COLUMN_COUNT):
-                x = column * (WIDTH + MARGIN) + (WIDTH / 2 + MARGIN)
-                y = row * (HEIGHT + MARGIN) + (HEIGHT / 2 + MARGIN)
-                sprite = arcade.SpriteSolidColor(
-                    WIDTH, HEIGHT, self.get_cell_color(row, column)
-                )
-                sprite.center_x = x
-                sprite.center_y = y
+                x, y = self.grid_to_central_coordinate(row, column)
+                sprite = arcade.SpriteSolidColor(WIDTH, HEIGHT, arcade.color.WHITE)
+                sprite.color = self.get_cell_color(row,column)
+                sprite.center_x, sprite.center_y = x, y
                 self.grid_sprite_list.append(sprite)
                 self.grid_sprites[row].append(sprite)
 
@@ -74,8 +87,7 @@ class Game(arcade.Window):
         """
 
         # Convert the clicked mouse position into grid coordinates
-        column = int(x // (WIDTH + MARGIN))
-        row = int(y // (HEIGHT + MARGIN))
+        row, column = self.coordinate_to_grid(x, y)
 
         # Make sure we are on-grid. It is possible to click in the upper right
         # corner in the margin and go to a grid location that doesn't exist
@@ -84,18 +96,20 @@ class Game(arcade.Window):
             return
 
         print(
-            f"Click coordinates: ({x}, {y}). Grid coordinates: ({row}, {column}), Height: {self.grid[row][column]}"
+            f"Click coordinates: ({x}, {y}). Grid coordinates: ({row}, {column}), Height: {self.grid_height[row][column]}, Color: {self.grid_sprites[row][column].color}"
         )
 
-        # Flip the location between 1 and 0.
-        # if self.grid[row][column] == 0:
-        #     self.grid[row][column] = 1
-        # else:
-        #     self.grid[row][column] = 0
+        # Flip the color of the sprite
+        # self.grid_selected=[row,column]
+        if self.grid_selected:
+            srow,scol=self.grid_selected
+            self.grid_sprites[srow][scol].color=self.get_cell_color(srow,scol)
+        self.grid_sprites[row][column].color=mix_color(self.grid_sprites[row][column].color,arcade.color.GRAY)
+        self.grid_selected=[row,column]
 
 
 def main():
-    game = Game(SCREEN_WIDTH, SCREEN_HEIGHT, "The Action")
+    Game(SCREEN_WIDTH, SCREEN_HEIGHT, "The Action")
     arcade.run()
 
 
