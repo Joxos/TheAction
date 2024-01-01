7318e10d3798ffefb4f10550d50e548d56c52ef4
Joxos <johndoe@example.com>
2024-01-01 08:33:13 +0800

    add utils.py

--- the_action.py
+++ the_action.py
@@ -1,22 +1,7 @@
 from map_generator import generate_map
 import arcade
 from config import *
-
-
-def mix_color(a, b):
-    return ((a[0] + b[0]) // 2, (a[1] + b[1]) // 2, (a[2] + b[2] // 2))
-
-
-def grid_to_central_coordinate(row, column):
-    x = column * (WIDTH + BOARDER) + (WIDTH / 2 + BOARDER)
-    y = row * (HEIGHT + BOARDER) + (HEIGHT / 2 + BOARDER)
-    return [x, y]
-
-
-def coordinate_to_grid(x, y):
-    column = int(x // (WIDTH + BOARDER))
-    row = int(y // (HEIGHT + BOARDER))
-    return [row, column]
+from utils import mix_color, grid_to_central_coordinate, coordinate_to_grid
 
 
 class Game(arcade.Window):
---
+++ utils.py
@@ -0,0 +1,17 @@
+from config import *
+
+
+def mix_color(a, b):
+    return ((a[0] + b[0]) // 2, (a[1] + b[1]) // 2, (a[2] + b[2] // 2))
+
+
+def grid_to_central_coordinate(row, column):
+    x = column * (WIDTH + BOARDER) + (WIDTH / 2 + BOARDER)
+    y = row * (HEIGHT + BOARDER) + (HEIGHT / 2 + BOARDER)
+    return [x, y]
+
+
+def coordinate_to_grid(x, y):
+    column = int(x // (WIDTH + BOARDER))
+    row = int(y // (HEIGHT + BOARDER))
+    return [row, column]
