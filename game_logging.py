aa7a2ed04374befcfe6964b43b28ee44552fa2b5
Joxos <johndoe@example.com>
2024-01-27 11:16:48 +0800

    add key logging

--- game_logging.py
+++ game_logging.py
@@ -1,7 +1,8 @@
 from loguru import logger
-from events import OnMouseRelease, EventsManager
+from events import OnMouseRelease, EventsManager, OnKeyPress, OnKeyRelease
 from config import SIDEBAR_WIDTH, GRID_WIDTH, BOARDER_WIDTH
 from utils import coordinate_to_grid
+import arcade
 
 
 def log_mouse_release(game, event: OnMouseRelease, em: EventsManager):
@@ -15,4 +16,23 @@ def log_mouse_release(game, event: OnMouseRelease, em: EventsManager):
     )
 
 
-subscriptions = {OnMouseRelease: log_mouse_release}
+def get_key_by_value(value):
+    for key, val in arcade.key.__dict__.items():
+        if val == value:
+            return key
+    return None
+
+
+def log_key_press(game, event: OnKeyPress, em: EventsManager):
+    logger.debug(f"Key pressed: {get_key_by_value(event.key)}")
+
+
+def log_key_release(game, event: OnKeyRelease, em: EventsManager):
+    logger.debug(f"Key released: {get_key_by_value(event.key)}")
+
+
+subscriptions = {
+    OnMouseRelease: log_mouse_release,
+    OnKeyPress: log_key_press,
+    OnKeyRelease: log_key_release,
+}
