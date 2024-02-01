9b42214acca4839620745bf5fb74a3906e7be4c9
Joxos <johndoe@example.com>
2024-02-01 19:39:38 +0800

    update logging in events.py

--- events.py
+++ events.py
@@ -7,67 +7,6 @@ class Event:
         pass
 
 
-class EventsManager:
-    def __init__(self):
-        self.events = {}
-        self.game_ref = None
-
-    def set_game_ref(self, game_ref):
-        self.game_ref = game_ref
-
-    def register(self, event):
-        if isinstance(event, list):
-            for e in event:
-                logger.debug(f"Registering event: {e.__name__}")
-                self.events[e] = []
-        else:
-            logger.debug(f"Registering event: {event}")
-            self.events[event] = []
-
-    def subscribe(self, event, func):
-        logger.debug(f"{event.__name__} -> {func.__name__}")
-        if isinstance(event, list):
-            for f in func:
-                self.events[event].append(f)
-        else:
-            self.events[event].append(func)
-
-    def multi_subscribe(self, subscriptions):
-        for event, func in subscriptions.items():
-            self.subscribe(event, func)
-
-    def new_event(self, new_event):
-        event_type = type(new_event)
-        # logger.debug(f"New event: {new_event}")
-        for event, func_list in self.events.items():
-            if event == event_type:
-                for func in func_list:
-                    # logger.debug(f"Calling function: {func} with event: {new_event}")
-                    func(self.game_ref, new_event, self)
-                return
-
-    def import_module(self, name):
-        logger.debug(f"Importing module: {name}")
-        module = importlib.import_module(name)
-        if hasattr(module, "subscriptions"):
-            logger.debug(f"Found subscriptions in module: {name}")
-            self.multi_subscribe(module.subscriptions)
-        if hasattr(module, "registrations"):
-            logger.debug(f"Found registrations in module: {name}")
-            self.register(module.registrations)
-
-    def import_modules(self, names):
-        for name in names:
-            self.import_module(name)
-
-    def verbose_subscription_info(self):
-        logger.info("Event subscriptions:")
-        for event, func_list in self.events.items():
-            logger.info(f"{event.__name__}:")
-            for func in func_list:
-                logger.info(f"  {func.__name__}")
-
-
 class OnMouseMotion(Event):
     def __init__(self, x, y, delta_x, delta_y):
         self.x = x
@@ -147,7 +86,7 @@ class OnCellSelected(Event):
         self.column = column
 
 
-default_events_list = [
+DEFAULT_EVENT_LIST = [
     OnDraw,
     OnMouseMotion,
     OnLeftMousePress,
@@ -162,3 +101,70 @@ default_events_list = [
     OnKeyRelease,
     OnCellSelected,
 ]
+
+IGNORED_EVENTS_LOGGING = [OnUpdate, OnDraw, OnMouseMotion]
+
+
+class EventsManager:
+    def __init__(self):
+        self.events = {}
+        self.game_ref = None
+
+    def set_game_ref(self, game_ref):
+        self.game_ref = game_ref
+
+    def register(self, event):
+        if isinstance(event, list):
+            for e in event:
+                logger.debug(f"Registering event: {e.__name__}")
+                self.events[e] = []
+        else:
+            logger.debug(f"Registering event: {event}")
+            self.events[event] = []
+
+    def subscribe(self, event, func):
+        logger.debug(f"{event.__name__} -> {func.__name__}")
+        if isinstance(event, list):
+            for f in func:
+                self.events[event].append(f)
+        else:
+            self.events[event].append(func)
+
+    def multi_subscribe(self, subscriptions):
+        for event, func in subscriptions.items():
+            self.subscribe(event, func)
+
+    def new_event(self, new_event):
+        event_type = type(new_event)
+        if event_type not in IGNORED_EVENTS_LOGGING:
+            logger.debug(f"New event: {new_event.__class__.__name__}")
+        for event, func_list in self.events.items():
+            if event == event_type:
+                for func in func_list:
+                    if event_type not in IGNORED_EVENTS_LOGGING:
+                        logger.debug(
+                            f"Calling function: {func.__name__} with event: {new_event.__class__.__name__}"
+                        )
+                    func(self.game_ref, new_event, self)
+                return
+
+    def import_module(self, name):
+        logger.debug(f"Importing module: {name}")
+        module = importlib.import_module(name)
+        if hasattr(module, "subscriptions"):
+            logger.debug(f"Found subscriptions in module: {name}")
+            self.multi_subscribe(module.subscriptions)
+        if hasattr(module, "registrations"):
+            logger.debug(f"Found registrations in module: {name}")
+            self.register(module.registrations)
+
+    def import_modules(self, names):
+        for name in names:
+            self.import_module(name)
+
+    def verbose_subscription_info(self):
+        logger.info("Event subscriptions:")
+        for event, func_list in self.events.items():
+            logger.info(f"{event.__name__}:")
+            for func in func_list:
+                logger.info(f"  {func.__name__}")
